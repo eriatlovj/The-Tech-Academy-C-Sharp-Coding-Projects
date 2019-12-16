1bb7cf0e7bbb5b3e317a60f3113b7064c2fe4e45
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-12-15 18:34:21 -0800

    Drill Step 248
    Submitted drill step 248 Code First MVC

---
+++ CodeFirstMVC/CodeFirstMVC/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstMVC.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class StudentDBContext : DbContext
+    {
+        public StudentDBContext()
+        { 
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
